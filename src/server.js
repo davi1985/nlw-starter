7086009efed6d5d03e2ebb0c9430c2c8f68aed7d
evandersondev <evandersonvasconcelos23@gmail.com>
2020-06-07 22:32:16 -0300

    using module path

--- src/server.js
+++ src/server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const server = express();
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
+const { resolve } = require('path');
 
 server.use(routes);
 
 // setting express
-server.use(express.static('public'));
+server.use(express.static(resolve(__dirname, '..', 'public')));
 server.use(express.urlencoded({ extended: true }));
 
 // nunjucks settings
-nunjucks.configure('src/views', {
+nunjucks.configure(resolve(__dirname, 'views'), {
   express: server,
   noCache: true,
 });
